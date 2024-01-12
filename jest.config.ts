2ba0f92387973dab2743add30fbb896c9b10e138
kistgab <gabrielkist02@gmail.com>
2024-01-12 11:16:36 -0300

    chore: add jest, husky and lint-staged

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from "jest";
+import { pathsToModuleNameMapper } from "ts-jest";
+import { compilerOptions } from "./tsconfig.json";
+
+const config: Config = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+  coverageDirectory: "coverage",
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+  testEnvironment: "node",
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+};
+
+export default config;
