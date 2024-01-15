793528a862000abb5d354f0e4e208c5e6141bfda
kistgab <gabrielkist02@gmail.com>
2024-01-15 08:58:35 -0300

    feat: add signup verification for the name property

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+export default class SignUpController {
+  handle(httpRequest: any): any {
+    httpRequest;
+    return {
+      statusCode: 422,
+    };
+  }
+}
