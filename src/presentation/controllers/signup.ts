708be5dd31dcb471f43665552670e7b028db66b4
kistgab <gabrielkist02@gmail.com>
2024-01-15 09:20:38 -0300

    feat: make email required in signup controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,19 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 export default class SignUpController {
   handle(httpRequest: any): any {
-    httpRequest;
-    return {
-      statusCode: 422,
-      body: new Error("Missing param: name"),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 422,
+        body: new Error("Missing param: name"),
+      };
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 422,
+        body: new Error("Missing param: email"),
+      };
+    }
   }
 }
