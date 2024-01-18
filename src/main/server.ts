de053fa15812fa1ea4ff8adce982c3496b3f3021
kistgab <gabrielkist02@gmail.com>
2024-01-18 10:05:19 -0300

    refactor: move app to a specific file

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,6 @@
-import express from "express";
+import app from "./config/app";
 
-const app = express();
 const port = 3000;
-
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
