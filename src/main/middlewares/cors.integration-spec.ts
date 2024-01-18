e58dcebc92753489bd147d5557fd2cf5a2799e2b
kistgab <gabrielkist02@gmail.com>
2024-01-18 10:58:37 -0300

    feat: ensure cors is enable

---
+++ src/main/middlewares/cors.integration-spec.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+import app from "../config/app";
+
+describe("Cors Middleware", () => {
+  it("should enable cors", async () => {
+    app.post("/cors_test", (req, res) => {
+      res.send(req.body);
+    });
+    await request(app)
+      .get("/cors_test")
+      .expect("access-control-allow-origin", "*")
+      .expect("access-control-allow-headers", "*")
+      .expect("access-control-allow-methods", "*");
+  });
+});
