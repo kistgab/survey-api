207dde8934addaf51adb0444709f7641ec607832
kistgab <gabrielkist02@gmail.com>
2024-01-22 12:09:18 -0300

    test: ensure LoginController calls EmailValidator

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,14 +1,26 @@
 import MissingParamError from "../../errors/missing-param-error";
 import { unprocessableContent } from "../../helpers/http-helper";
+import EmailValidator from "../../protocols/email-validator";
 import LoginController from "./login";
 
 type CreateSutType = {
   sut: LoginController;
+  emailValidatorStub: EmailValidator;
 };
 
+function createEmailValidatorStub(): EmailValidator {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+}
+
 function createSut(): CreateSutType {
-  const sut = new LoginController();
-  return { sut };
+  const emailValidatorStub = createEmailValidatorStub();
+  const sut = new LoginController(emailValidatorStub);
+  return { sut, emailValidatorStub };
 }
 
 describe("Login Controller", () => {
@@ -38,5 +50,20 @@ describe("Login Controller", () => {
     const result = await sut.handle(httpRequest);
 
     expect(result).toEqual(unprocessableContent(new MissingParamError("password")));
+  });
+
+  it("should call EmailValidator with correct email", async () => {
+    const { sut, emailValidatorStub } = createSut();
+    const httpRequest = {
+      body: {
+        password: "any_password",
+        email: "any_email@mail.com",
+      },
+    };
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+
+    await sut.handle(httpRequest);
+
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
 });
