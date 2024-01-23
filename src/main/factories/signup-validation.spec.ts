e2a00479c66591a5ccbd57e85eb9eb5ddddfe1cf
kistgab <gabrielkist02@gmail.com>
2024-01-23 13:30:55 -0300

    test: ensure SignUpValidationFactory creates the ValidationComposite with all the Validators

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,27 @@
+import { RequestSignUpBody } from "../../presentation/controllers/signup/signup";
+import RequiredFieldValidation from "../../presentation/helpers/validators/required-field-validation";
+import Validation from "../../presentation/helpers/validators/validation";
+import ValidationComposite from "../../presentation/helpers/validators/validation-composite";
+import SignUpValidationFactory from "./signup-validation";
+
+type RequestSignUpBodyKeys = keyof RequestSignUpBody;
+jest.mock("../../presentation/helpers/validators/validation-composite");
+
+describe("SignUp Validation Factory", () => {
+  it("should call ValidationComposite with all validations", () => {
+    const validations: Validation<unknown>[] = [];
+    const requiredFields: RequestSignUpBodyKeys[] = [
+      "name",
+      "password",
+      "email",
+      "passwordConfirmation",
+    ];
+    for (const field of requiredFields) {
+      validations.push(new RequiredFieldValidation<RequestSignUpBody>(field));
+    }
+
+    SignUpValidationFactory.create();
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations);
+  });
+});
