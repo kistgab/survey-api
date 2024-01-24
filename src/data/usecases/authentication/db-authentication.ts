435de9e52dfd1a3b3964f6d1a967afd5bd18f25e
kistgab <gabrielkist02@gmail.com>
2024-01-24 12:06:00 -0300

    feat: ensure db authentication calls FindAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,12 @@
+import { InputAuthenticationDto } from "../../../domain/dtos/authentication-dto";
+import Authentication from "../../../domain/usecases/authentication";
+import FindAccountByEmailRepository from "../../protocols/find-account-by-email-repository";
+
+export default class DbAuthentication implements Authentication {
+  constructor(private readonly findAccountByEmailRepository: FindAccountByEmailRepository) {}
+
+  async auth(input: InputAuthenticationDto): Promise<string | null> {
+    await this.findAccountByEmailRepository.find(input.email);
+    return null;
+  }
+}
