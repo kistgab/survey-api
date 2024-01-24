970f8c1d7deeff0997bf588a8acc49285ad6f8a4
kistgab <gabrielkist02@gmail.com>
2024-01-24 12:15:48 -0300

    refactor: move object creations to helper factory methods

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,29 +1,53 @@
+import { InputAuthenticationDto } from "../../../domain/dtos/authentication-dto";
 import AccountModel from "../../models/account-model";
 import FindAccountByEmailRepository from "../../protocols/find-account-by-email-repository";
 import DbAuthentication from "./db-authentication";
 
+function createFakeAccount(): AccountModel {
+  return {
+    id: "any_id",
+    name: "any_name",
+    email: "any_email@mail.com",
+    password: "hashed_password",
+  };
+}
+
+function createFindAccountByEmailRepository(): FindAccountByEmailRepository {
+  class FindAccountByEmailRepositoryStub implements FindAccountByEmailRepository {
+    async find(): Promise<AccountModel> {
+      return Promise.resolve(createFakeAccount());
+    }
+  }
+  return new FindAccountByEmailRepositoryStub();
+}
+
+function createFakeInputDto(): InputAuthenticationDto {
+  return {
+    email: "any_email@mail.com",
+    password: "any_password",
+  };
+}
+
+type SutTypes = {
+  sut: DbAuthentication;
+  findAccountByEmailRepositoryStub: FindAccountByEmailRepository;
+};
+
+function createSut(): SutTypes {
+  const findAccountByEmailRepositoryStub = createFindAccountByEmailRepository();
+  const sut = new DbAuthentication(findAccountByEmailRepositoryStub);
+  return {
+    sut,
+    findAccountByEmailRepositoryStub,
+  };
+}
+
 describe("DbAuthentication UseCase", () => {
   it("should call FindAccountByEmailRepository with correct email", async () => {
-    class FindAccountByEmailRepositoryStub implements FindAccountByEmailRepository {
-      async find(email: string): Promise<AccountModel> {
-        const account: AccountModel = {
-          id: "any_id",
-          name: "any_name",
-          email,
-          password: "hashed_password",
-        };
-        return Promise.resolve(account);
-      }
-    }
-    const findAccountByEmailRepositoryStub = new FindAccountByEmailRepositoryStub();
+    const { sut, findAccountByEmailRepositoryStub } = createSut();
     const findSpy = jest.spyOn(findAccountByEmailRepositoryStub, "find");
 
-    const sut = new DbAuthentication(findAccountByEmailRepositoryStub);
-
-    await sut.auth({
-      email: "any_email@mail.com",
-      password: "any_password",
-    });
+    await sut.auth(createFakeInputDto());
 
     expect(findSpy).toHaveBeenCalledWith("any_email@mail.com");
   });
