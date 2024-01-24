69804e26f3b26f53b614a687139d3ae939c31c26
kistgab <gabrielkist02@gmail.com>
2024-01-24 11:00:04 -0300

    refactor: move Sut and ValidationStub creation to a helper factory method

--- src/presentation/helpers/validators/composite/validation-composite.spec.ts
+++ src/presentation/helpers/validators/composite/validation-composite.spec.ts
@@ -2,15 +2,30 @@ import MissingParamError from "../../../errors/missing-param-error";
 import Validation from "../validation";
 import ValidationComposite from "./validation-composite";
 
+type SutTypes = {
+  sut: ValidationComposite<unknown>;
+  validationStub: Validation<unknown>;
+};
+
+function createValidationStub(): Validation<unknown> {
+  class ValidationStub implements Validation<unknown> {
+    validate(): Error | undefined {
+      return;
+    }
+  }
+  return new ValidationStub();
+}
+
+function createSut(): SutTypes {
+  const validationStub = createValidationStub();
+  const sut = new ValidationComposite([validationStub]);
+  return { sut, validationStub };
+}
+
 describe("Validation Composite", () => {
   it("should return the same error that the internal validation returns", () => {
-    class ValidationStub implements Validation<unknown> {
-      validate(): Error | undefined {
-        return new MissingParamError("field");
-      }
-    }
-    const validations = [new ValidationStub()];
-    const sut = new ValidationComposite(validations);
+    const { sut, validationStub } = createSut();
+    jest.spyOn(validationStub, "validate").mockReturnValueOnce(new MissingParamError("field"));
 
     const error = sut.validate({ field: "any_value" });
 
