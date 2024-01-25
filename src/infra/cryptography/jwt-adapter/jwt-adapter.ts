f19691dbc8c913109cec03ea91db0be37fae83f4
kistgab <gabrielkist02@gmail.com>
2024-01-25 10:29:56 -0300

    refactor: remove unecessary code

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -6,6 +6,6 @@ export default class JwtAdapter implements Encrypter {
 
   async encrypt(value: string): Promise<string> {
     const token = jwt.sign({ id: value }, this.secretKey);
-    return token;
+    return Promise.resolve(token);
   }
 }
