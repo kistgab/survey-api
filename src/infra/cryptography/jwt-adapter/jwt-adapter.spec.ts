37a2e4463864d614141eb85b499836ba5a462f92
kistgab <gabrielkist02@gmail.com>
2024-01-25 10:30:30 -0300

    test: ensure JwtAdapter returns the generated token on success

--- src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,7 +2,7 @@ import * as jwt from "jsonwebtoken";
 import JwtAdapter from "./jwt-adapter";
 
 jest.mock("jsonwebtoken", () => ({
-  sign: jest.fn().mockReturnValue("jwt"),
+  sign: jest.fn().mockReturnValue("any_jwt"),
 }));
 
 describe("JWT Adapter", () => {
@@ -13,5 +13,13 @@ describe("JWT Adapter", () => {
     await sut.encrypt("any_id");
 
     expect(signSpy).toHaveBeenCalledWith({ id: "any_id" }, "secret_key");
+  });
+
+  it("should return a token on sign success", async () => {
+    const sut = new JwtAdapter("secret_key");
+
+    const token = await sut.encrypt("any_id");
+
+    expect(token).toBe("any_jwt");
   });
 });
