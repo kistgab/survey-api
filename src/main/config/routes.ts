f751c6d0d8953dd59881d01260417d609add1a45
kistgab <gabrielkist02@gmail.com>
2024-01-29 10:01:29 -0300

    feat: add login route

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,8 @@
 import { Express, Router } from "express";
-import signupRoute from "../routes/authentication-routes";
+import authenticationRoutes from "../routes/authentication-routes";
 
 export default function setupRoutes(app: Express): void {
   const router = Router();
   app.use("/api", router);
-  signupRoute(router);
+  authenticationRoutes(router);
 }
--- src/main/routes/authentication-routes.ts
+++ src/main/routes/authentication-routes.ts
@@ -2,9 +2,13 @@ import { Router } from "express";
 import { OutputAddAccountDto } from "../../domain/dtos/add-account-dto";
 import { RequestSignUpBody } from "../../presentation/controllers/signup/signup-controller";
 import adaptRoute from "../adapters/express/express-route-adapter";
+import LoginControllerFactory from "../factories/login/login-controller-factory";
 import SignUpControllerFactory from "../factories/signup/signup-controller-factory";
 
-export default (router: Router): void => {
-  const controller = SignUpControllerFactory.create();
-  router.post("/signup", adaptRoute<RequestSignUpBody, Error | OutputAddAccountDto>(controller));
-};
+export default function authenticationRoutes(router: Router): void {
+  router.post(
+    "/signup",
+    adaptRoute<RequestSignUpBody, Error | OutputAddAccountDto>(SignUpControllerFactory.create()),
+  );
+  router.post("/login", adaptRoute(LoginControllerFactory.create()));
+}
