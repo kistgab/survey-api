b202997a6523fed6f730a51f40e40af464b45901
kistgab <gabrielkist02@gmail.com>
2024-02-07 09:53:43 -0300

    feat: add Validation creation in SignUpControllerFactory

--- src/main/factories/signup/signup-controller-factory.ts
+++ src/main/factories/signup/signup-controller-factory.ts
@@ -1,6 +1,8 @@
 import DbAddAccount from "../../../data/usecases/add-account/db-add-account";
+import DbAuthentication from "../../../data/usecases/authentication/db-authentication";
 import { OutputAddAccountDto } from "../../../domain/dtos/add-account-dto";
 import BCryptAdapter from "../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter";
+import JwtAdapter from "../../../infra/cryptography/jwt-adapter/jwt-adapter";
 import { AccountMongoRepository } from "../../../infra/db/mongodb/account/account-mongo-repository";
 import LogMongoRepository from "../../../infra/db/mongodb/log/log-mongo-repository";
 import SignUpController, {
@@ -17,7 +19,14 @@ export default abstract class SignUpControllerFactory {
     const accountRepository = new AccountMongoRepository();
     const addAccount = new DbAddAccount(hasher, accountRepository);
     const validation = SignUpValidationFactory.create();
-    const signUpController = new SignUpController(addAccount, validation);
+    const encrypter = new JwtAdapter(process.env.JWT_SECRET);
+    const authentication = new DbAuthentication(
+      accountRepository,
+      hasher,
+      encrypter,
+      accountRepository,
+    );
+    const signUpController = new SignUpController(addAccount, validation, authentication);
     const logErrorRepository = new LogMongoRepository();
     return new LogControllerDecorator(signUpController, logErrorRepository);
   }
