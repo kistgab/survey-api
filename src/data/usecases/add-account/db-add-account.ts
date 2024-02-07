5145b99bf8465a3060d66c8f159d819a3d224e13
kistgab <gabrielkist02@gmail.com>
2024-02-07 11:14:21 -0300

    feat: ensure DbAddAccount calls FindAccountByEmailRepository with correct value

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -2,14 +2,17 @@ import { InputAddAccountDto, OutputAddAccountDto } from "../../../domain/dtos/ad
 import { AddAccount } from "../../../domain/usecases/add-account";
 import Hasher from "../../protocols/cryptography/hasher";
 import AddAccountRepository from "../../protocols/db/account/add-account-repository";
+import FindAccountByEmailRepository from "../../protocols/db/account/find-account-by-email-repository";
 
 export default class DbAddAccount implements AddAccount {
   constructor(
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
+    private readonly findAccountByEmailRepository: FindAccountByEmailRepository,
   ) {}
 
   async add(account: InputAddAccountDto): Promise<OutputAddAccountDto> {
+    await this.findAccountByEmailRepository.findByEmail(account.email);
     const encryptedPassword = await this.hasher.hash(account.password);
     const createdAccount = await this.addAccountRepository.add({
       email: account.email,
