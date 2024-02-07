138d613708ca44ba921f607f4cf0c7b96903eed5
kistgab <gabrielkist02@gmail.com>
2024-02-07 10:04:18 -0300

    fix: change controller return type to the new one

--- src/main/factories/signup/signup-controller-factory.ts
+++ src/main/factories/signup/signup-controller-factory.ts
@@ -1,19 +1,19 @@
 import DbAddAccount from "../../../data/usecases/add-account/db-add-account";
 import DbAuthentication from "../../../data/usecases/authentication/db-authentication";
-import { OutputAddAccountDto } from "../../../domain/dtos/add-account-dto";
 import BCryptAdapter from "../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter";
 import JwtAdapter from "../../../infra/cryptography/jwt-adapter/jwt-adapter";
 import { AccountMongoRepository } from "../../../infra/db/mongodb/account/account-mongo-repository";
 import LogMongoRepository from "../../../infra/db/mongodb/log/log-mongo-repository";
 import SignUpController, {
   RequestSignUpBody,
+  ResponseSignUpBody,
 } from "../../../presentation/controllers/signup/signup-controller";
 import Controller from "../../../presentation/protocols/controller";
 import LogControllerDecorator from "../../decorators/log-controller/log-controller-decorator";
 import SignUpValidationFactory from "./signup-validation-factory";
 
 export default abstract class SignUpControllerFactory {
-  static create(): Controller<RequestSignUpBody, Error | OutputAddAccountDto> {
+  static create(): Controller<RequestSignUpBody, Error | ResponseSignUpBody> {
     const salt = 12;
     const hasher = new BCryptAdapter(salt);
     const accountRepository = new AccountMongoRepository();
--- src/main/routes/authentication-routes.ts
+++ src/main/routes/authentication-routes.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
-import { OutputAddAccountDto } from "../../domain/dtos/add-account-dto";
-import { RequestSignUpBody } from "../../presentation/controllers/signup/signup-controller";
 import adaptRoute from "../adapters/express/express-route-adapter";
 import LoginControllerFactory from "../factories/login/login-controller-factory";
 import SignUpControllerFactory from "../factories/signup/signup-controller-factory";
 
 export default function authenticationRoutes(router: Router): void {
-  router.post(
-    "/signup",
-    adaptRoute<RequestSignUpBody, Error | OutputAddAccountDto>(SignUpControllerFactory.create()),
-  );
+  router.post("/signup", adaptRoute(SignUpControllerFactory.create()));
   router.post("/login", adaptRoute(LoginControllerFactory.create()));
 }
