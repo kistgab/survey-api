b2abc8b63093d6bc49a65dce8c0c76005f49da3b
kistgab <gabrielkist02@gmail.com>
2024-02-08 21:42:50 -0300

    test: ensure AuthMiddleware calls FindAccountByTokenRepository with the correct values

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -1,13 +1,56 @@
+import AccountModel from "../../../data/models/account-model";
+import FindAccountByTokenRepository from "../../../data/protocols/db/account/find-account-by-token-repository";
 import AccessDeniedError from "../../errors/access-denied-error";
 import { forbidden } from "../../helpers/http/http-helper";
 import { AuthMiddleware } from "./auth-middleware";
 
+function createFakeAccount(): AccountModel {
+  return {
+    id: "any_id",
+    email: "any_email@mail.com",
+    password: "hashed_password",
+    name: "any_name",
+  };
+}
+
+function createFindAccountByTokenRepositoryStub(): FindAccountByTokenRepository {
+  class FindAccountByTokenRepositoryStub implements FindAccountByTokenRepository {
+    async findByToken(): Promise<AccountModel | null> {
+      return Promise.resolve(createFakeAccount());
+    }
+  }
+  return new FindAccountByTokenRepositoryStub();
+}
+
+type SutTypes = {
+  sut: AuthMiddleware;
+  findAccountByTokenRepositoryStub: FindAccountByTokenRepository;
+};
+
+function createSut(): SutTypes {
+  const findAccountByTokenRepositoryStub = createFindAccountByTokenRepositoryStub();
+  const sut = new AuthMiddleware(findAccountByTokenRepositoryStub);
+  return {
+    sut,
+    findAccountByTokenRepositoryStub,
+  };
+}
+
 describe("Auth Middleware", () => {
   it("should return 403 if no x-access-token is provided in headers", async () => {
-    const sut = new AuthMiddleware();
+    const { sut } = createSut();
 
     const result = await sut.handle({});
 
     expect(result).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it("should call FindAccountByTokenRepository with correct accessToken", async () => {
+    const { sut, findAccountByTokenRepositoryStub } = createSut();
+    const findAccountByTokenSpy = jest.spyOn(findAccountByTokenRepositoryStub, "findByToken");
+
+    await sut.handle({ headers: { "x-access-token": "any_token" } });
+
+    expect(findAccountByTokenSpy).toHaveBeenCalledWith("any_token");
   });
 });
