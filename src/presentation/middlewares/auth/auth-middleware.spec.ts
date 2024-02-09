6603c4f43c74a4b387c2ce8917b1d1feda5425d3
kistgab <gabrielkist02@gmail.com>
2024-02-09 09:30:03 -0300

    test: ensure AuthMiddleware return 200 if FindAccountByTokenRepository succeds

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -1,8 +1,13 @@
 import AccountModel from "../../../data/models/account-model";
 import FindAccountByTokenRepository from "../../../data/protocols/db/account/find-account-by-token-repository";
 import AccessDeniedError from "../../errors/access-denied-error";
-import { forbidden } from "../../helpers/http/http-helper";
+import { forbidden, ok } from "../../helpers/http/http-helper";
+import { HttpRequest } from "../../protocols/http";
 import { AuthMiddleware } from "./auth-middleware";
+
+function createFakeRequest(): HttpRequest<unknown> {
+  return { headers: { "x-access-token": "any_token" } };
+}
 
 function createFakeAccount(): AccountModel {
   return {
@@ -49,7 +54,7 @@ describe("Auth Middleware", () => {
     const { sut, findAccountByTokenRepositoryStub } = createSut();
     const findAccountByTokenSpy = jest.spyOn(findAccountByTokenRepositoryStub, "findByToken");
 
-    await sut.handle({ headers: { "x-access-token": "any_token" } });
+    await sut.handle(createFakeRequest());
 
     expect(findAccountByTokenSpy).toHaveBeenCalledWith("any_token");
   });
@@ -60,8 +65,16 @@ describe("Auth Middleware", () => {
       .spyOn(findAccountByTokenRepositoryStub, "findByToken")
       .mockReturnValueOnce(Promise.resolve(null));
 
-    const result = await sut.handle({});
+    const result = await sut.handle(createFakeRequest());
 
     expect(result).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it("should return 200 if FindAccountByTokenRepository returns an account", async () => {
+    const { sut } = createSut();
+
+    const result = await sut.handle(createFakeRequest());
+
+    expect(result).toEqual(ok({ accountId: "any_id" }));
   });
 });
