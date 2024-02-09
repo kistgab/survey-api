10669607c167f8c614ba52f8a7ebf2c5de4c1b37
kistgab <gabrielkist02@gmail.com>
2024-02-09 10:49:38 -0300

    refactor: move sut creation to a factory helper method

--- src/data/usecases/find-account-by-token/db-find-account-by-token.spec.ts
+++ src/data/usecases/find-account-by-token/db-find-account-by-token.spec.ts
@@ -1,14 +1,33 @@
 import Decrypter from "../../protocols/cryptography/decrypter";
 import DbFindAccountByToken from "./db-find-account-by-token";
+
+type SutTypes = {
+  sut: DbFindAccountByToken;
+  decrypterStub: Decrypter;
+};
+
+function createDecrypter(): Decrypter {
+  class DecrypterStub implements Decrypter {
+    async decrypt(): Promise<string> {
+      return Promise.resolve("any_value");
+    }
+  }
+  const decrypterStub = new DecrypterStub();
+  return decrypterStub;
+}
+
+function createSut(): SutTypes {
+  const decrypterStub = createDecrypter();
+  const sut = new DbFindAccountByToken(decrypterStub);
+  return {
+    sut,
+    decrypterStub,
+  };
+}
+
 describe("DbFindAccountByToken UseCase", () => {
   it("should call Decrypter with correct values", async () => {
-    class DecrypterStub implements Decrypter {
-      async decrypt(): Promise<string> {
-        return Promise.resolve("any_value");
-      }
-    }
-    const decrypterStub = new DecrypterStub();
-    const sut = new DbFindAccountByToken(decrypterStub);
+    const { sut, decrypterStub } = createSut();
     const decryptSpy = jest.spyOn(decrypterStub, "decrypt");
 
     await sut.findByToken("any_token");
