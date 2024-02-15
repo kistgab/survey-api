2cb5fc4be9b5c12fe8de7757cef55a005ec9891f
kistgab <gabrielkist02@gmail.com>
2024-02-14 23:36:40 -0300

    feat: ensure ListSurveyController returns 204 on success with no content

--- src/presentation/controllers/survey/list-surveys/list-surveys-controller.ts
+++ src/presentation/controllers/survey/list-surveys/list-surveys-controller.ts
@@ -1,17 +1,20 @@
 import { SurveyModel } from "../../../../data/models/survey-model";
 import { ListSurveys } from "../../../../domain/usecases/list-surveys";
-import { internalServerError, ok } from "../../../helpers/http/http-helper";
+import { internalServerError, noContent, ok } from "../../../helpers/http/http-helper";
 import Controller from "../../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../../protocols/http";
 
-export default class ListSurveysController implements Controller<unknown, SurveyModel[]> {
+export default class ListSurveysController implements Controller<unknown, SurveyModel[] | null> {
   constructor(private readonly listSurveys: ListSurveys) {}
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse<SurveyModel[] | Error>> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse<SurveyModel[] | Error | null>> {
     try {
       httpRequest;
       const surveys = await this.listSurveys.list();
-      return ok(surveys);
+      if (surveys.length) {
+        return ok(surveys);
+      }
+      return noContent();
     } catch (error) {
       return internalServerError(error as Error);
     }
