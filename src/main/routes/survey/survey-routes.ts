f3e8814f9164c375cb440e6bb3676d72efc8cddf
kistgab <gabrielkist02@gmail.com>
2024-02-15 21:49:28 -0300

    feat: ensure List Surveys route returns 403 when no accessToken is provided

--- src/main/routes/survey/survey-routes.ts
+++ src/main/routes/survey/survey-routes.ts
@@ -2,9 +2,12 @@ import { Router } from "express";
 import adaptMiddleware from "../../adapters/express-middleware-adapter";
 import adaptRoute from "../../adapters/express-route-adapter";
 import AddSurveyControllerFactory from "../../factories/controllers/survey/add-survey/add-survey-controller-factory";
+import ListSurveysControllerFactory from "../../factories/controllers/survey/list-surveys/list-surveys-controller-factory";
 import AuthMiddlewareFactory from "../../factories/middlewares/auth-middleware-factory";
 
 export default function surveyRoutes(router: Router): void {
   const adminAuth = adaptMiddleware(AuthMiddlewareFactory.create("admin"));
+  const auth = adaptMiddleware(AuthMiddlewareFactory.create());
   router.post("/surveys", adminAuth, adaptRoute(AddSurveyControllerFactory.create()));
+  router.get("/surveys", auth, adaptRoute(ListSurveysControllerFactory.create()));
 }
