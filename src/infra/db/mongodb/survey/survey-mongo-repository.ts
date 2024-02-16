fb44d461a80d30f58b8018a1b6776b2c29d25016
kistgab <gabrielkist02@gmail.com>
2024-02-15 21:04:10 -0300

    feat: ensure SurveyMongoRepository find all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,20 @@
-import { AddSurveyModel } from "../../../../data/models/survey-model";
+import { AddSurveyModel, SurveyModel } from "../../../../data/models/survey-model";
 import AddSurveyRepository from "../../../../data/protocols/db/survey/add-survey-repository";
+import { FindAllSurveysRepository } from "../../../../data/protocols/db/survey/find-all-surveys-repository";
 import { MongoHelper } from "../helpers/mongo-helper";
 
-export class SurveyMongoRepository implements AddSurveyRepository {
+export class SurveyMongoRepository implements AddSurveyRepository, FindAllSurveysRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveysCollection = MongoHelper.getCollection("surveys");
     await surveysCollection.insertOne(surveyData);
+  }
+
+  async findAll(): Promise<SurveyModel[]> {
+    const surveysCollection = MongoHelper.getCollection("surveys");
+    const surveys = await surveysCollection.find().toArray();
+    return surveys.map((survey) => {
+      const { _id, ...surveyWithoutId } = survey;
+      return { ...surveyWithoutId, id: _id.toString() } as SurveyModel;
+    });
   }
 }
