8258ab24c560fa142c29fa2d9b6e699e691304e0
kistgab <gabrielkist02@gmail.com>
2024-02-15 21:04:24 -0300

    test: ensure SurveyMongoRepository find all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.integration-spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.integration-spec.ts
@@ -44,4 +44,31 @@ describe("Survey Mongo Repository", () => {
       expect(survey?.date).toEqual(new Date());
     });
   });
+
+  describe("findAll", () => {
+    it("should load all surveys on success", async () => {
+      const survey1 = {
+        answers: [{ answer: "any_answer", image: "any_image" }],
+        question: "any_question",
+        date: new Date(),
+      };
+      const survey2 = {
+        answers: [{ answer: "another_answer", image: "another_image" }],
+        question: "another_question",
+        date: new Date(),
+      };
+      await surveyCollection.insertMany([{ ...survey1 }, { ...survey2 }]);
+      const sut = createSut();
+
+      const surveys = await sut.findAll();
+
+      const { id: id1, ...firstReturnedSurveyWithoutId } = surveys[0];
+      const { id: id2, ...secondReturnedSurveyWithoutId } = surveys[1];
+      expect(surveys.length).toBe(2);
+      expect(id1).toBeDefined();
+      expect(id2).toBeDefined();
+      expect(firstReturnedSurveyWithoutId).toEqual(survey1);
+      expect(secondReturnedSurveyWithoutId).toEqual(survey2);
+    });
+  });
 });
