a74f8dc599ddb6e5e7e6d83f308277c5aa97a2de
kistgab <gabrielkist02@gmail.com>
2024-02-27 11:24:43 -0300

    test: ensure DbListSurveyById calls FindByIdSurveysRepository with correct id

---
+++ src/data/usecases/survey/list-survey-by-id/db-list-survey-by-id.spec.ts
@@ -0,0 +1,53 @@
+import { SurveyModel } from "@src/data/models/survey-model";
+import { FindByIdSurveysRepository } from "@src/data/protocols/db/survey/find-by-id-surveys-repository";
+import { DbListSurveyById } from "@src/data/usecases/survey/list-survey-by-id/db-list-survey-by-id";
+import { ListSurveyById } from "../../../../domain/usecases/list-survey-by-id";
+
+function createFakeSurveys(): SurveyModel {
+  return {
+    question: "any_question",
+    answers: [
+      {
+        image: "any_image",
+        answer: "any_answer",
+      },
+    ],
+    date: new Date(),
+    id: "any_id",
+  };
+}
+
+function createFindByIdSurveysRepositoryStub(): FindByIdSurveysRepository {
+  class FindByIdSurveysRepositoryStub implements FindByIdSurveysRepository {
+    async findById(): Promise<SurveyModel> {
+      return Promise.resolve(createFakeSurveys());
+    }
+  }
+  return new FindByIdSurveysRepositoryStub();
+}
+
+type SutTypes = {
+  sut: ListSurveyById;
+  findByIdSurveysRepositoryStub: FindByIdSurveysRepository;
+};
+
+function createSut(): SutTypes {
+  const findByIdSurveysRepositoryStub = createFindByIdSurveysRepositoryStub();
+  const sut = new DbListSurveyById(findByIdSurveysRepositoryStub);
+
+  return {
+    sut,
+    findByIdSurveysRepositoryStub,
+  };
+}
+
+describe("DbListSurveyById UseCase", () => {
+  it("should call FindByIdSurveysRepository", async () => {
+    const { sut, findByIdSurveysRepositoryStub } = createSut();
+    const findByIdSpy = jest.spyOn(findByIdSurveysRepositoryStub, "findById");
+
+    await sut.list("any_id");
+
+    expect(findByIdSpy).toHaveBeenCalledTimes(1);
+  });
+});
