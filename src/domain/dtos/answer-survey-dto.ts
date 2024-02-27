b6f2e4b73f44b98a907072dc6557bb33669450b2
kistgab <gabrielkist02@gmail.com>
2024-02-27 11:01:59 -0300

    feat: add AnswerSurvey usecase

--- src/domain/dtos/answer-survey-dto.ts
+++ src/domain/dtos/answer-survey-dto.ts
@@ -1,3 +1,10 @@
+export type InputAnswerSurveyDto = {
+  surveyId: string;
+  accountId: string;
+  date: Date;
+  answer: string;
+};
+
 export type OutputAnswerSurveyDto = {
   id: string;
   surveyId: string;
