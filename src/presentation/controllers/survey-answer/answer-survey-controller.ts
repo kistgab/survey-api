aaaa0455dbcf6c9ece048b93da290db541f489ad
kistgab <gabrielkist02@gmail.com>
2024-03-04 09:42:09 -0300

    feat: ensure AnswerSurveyController returns 500 if ListSurveyById throes

--- src/presentation/controllers/survey-answer/answer-survey-controller.ts
+++ src/presentation/controllers/survey-answer/answer-survey-controller.ts
@@ -1,6 +1,9 @@
 import { InputAnswerSurveyDto, OutputAnswerSurveyDto } from "@src/domain/dtos/answer-survey-dto";
 import { ListSurveyById } from "@src/domain/usecases/survey/list-survey-by-id";
-import { unprocessableContent } from "@src/presentation/helpers/http/http-helper";
+import {
+  internalServerError,
+  unprocessableContent,
+} from "@src/presentation/helpers/http/http-helper";
 import Controller from "@src/presentation/protocols/controller";
 import { HttpRequest, HttpResponse } from "@src/presentation/protocols/http";
 
@@ -14,10 +17,14 @@ export class AnswerSurveyController
   async handle(
     httpRequest: HttpRequest<InputAnswerSurveyDto, AnswerSurveyParams>,
   ): Promise<HttpResponse<OutputAnswerSurveyDto | Error>> {
-    const { params } = httpRequest;
-    const survey = await this.listSurveyById.list(params?.surveyId || "");
-    if (!survey) return unprocessableContent(new Error("Survey not found"));
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    return null!;
+    try {
+      const { params } = httpRequest;
+      const survey = await this.listSurveyById.list(params?.surveyId || "");
+      if (!survey) return unprocessableContent(new Error("Survey not found"));
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      return null!;
+    } catch (error) {
+      return internalServerError(error as Error);
+    }
   }
 }
