e453a22fc5daadac7d9ce2993fba4c18c3fb4d84
kistgab <gabrielkist02@gmail.com>
2024-03-20 10:21:11 -0300

    feat: ensure DbLoadSurveyResult calls FindByIdRepository when survey has no answers

--- src/data/usecases/survey-answer/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-answer/load-survey-result/db-load-survey-result.spec.ts
@@ -1,23 +1,37 @@
 import { LoadSurveyResultRepository } from "@src/data/protocols/db/survey-answer/load-survey-result-repository";
+import { FindSurveyByIdRepository } from "@src/data/protocols/db/survey/find-by-id-surveys-repository";
+import { mockFindSurveyByIdRepository } from "@src/data/test/mock-db-survey";
 import { mockLoadSurveyResultRepository } from "@src/data/test/mock-db-survey-result";
 import { DbLoadSurveyResult } from "@src/data/usecases/survey-answer/load-survey-result/db-load-survey-result";
 import { mockSurveyResultModel } from "@src/domain/test/mock-survey";
+import * as Mockdate from "mockdate";
 
 type SutTypes = {
   sut: DbLoadSurveyResult;
   loadSurveyResultRepositoryStub: LoadSurveyResultRepository;
+  findSurveyByIdRepositoryStub: FindSurveyByIdRepository;
 };
 
 function createSut(): SutTypes {
   const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository();
-  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub);
+  const findSurveyByIdRepositoryStub = mockFindSurveyByIdRepository();
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub, findSurveyByIdRepositoryStub);
   return {
     sut,
     loadSurveyResultRepositoryStub,
+    findSurveyByIdRepositoryStub,
   };
 }
 
 describe("DbLoadSurveyResult UseCase", () => {
+  beforeAll(() => {
+    Mockdate.set(new Date());
+  });
+
+  afterAll(() => {
+    Mockdate.reset();
+  });
+
   it("Should call LoadSurveyResultRepository", async () => {
     const { sut, loadSurveyResultRepositoryStub } = createSut();
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, "loadBySurveyId");
@@ -42,5 +56,21 @@ describe("DbLoadSurveyResult UseCase", () => {
     const result = await sut.load("any_id");
 
     expect(result).toEqual(mockSurveyResultModel());
+  });
+
+  it("should call FindSurveyByIdRepository if LoadSurveyResultRepository returns null", async () => {
+    const {
+      sut,
+      loadSurveyResultRepositoryStub,
+      findSurveyByIdRepositoryStub: loadSurveyByIdRepositoryStub,
+    } = createSut();
+    jest
+      .spyOn(loadSurveyResultRepositoryStub, "loadBySurveyId")
+      .mockReturnValueOnce(Promise.resolve(null));
+    const findById = jest.spyOn(loadSurveyByIdRepositoryStub, "findById");
+
+    await sut.load("any_id");
+
+    expect(findById).toHaveBeenCalledWith("any_id");
   });
 });
--- src/data/usecases/survey-answer/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-answer/load-survey-result/db-load-survey-result.ts
@@ -1,12 +1,19 @@
 import { SurveyResultModel } from "@src/data/models/survey-result-model";
 import { LoadSurveyResultRepository } from "@src/data/protocols/db/survey-answer/load-survey-result-repository";
+import { FindSurveyByIdRepository } from "@src/data/protocols/db/survey/find-by-id-surveys-repository";
 import { LoadSurveyResult } from "@src/domain/usecases/survey-answer/load-survey-result";
 
 export class DbLoadSurveyResult implements LoadSurveyResult {
-  constructor(private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+  constructor(
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository,
+    private readonly findSurveyByIdRepository: FindSurveyByIdRepository,
+  ) {}
 
   async load(surveyId: string): Promise<SurveyResultModel | null> {
     const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId);
+    if (!surveyResult) {
+      await this.findSurveyByIdRepository.findById(surveyId);
+    }
     return surveyResult;
   }
 }
